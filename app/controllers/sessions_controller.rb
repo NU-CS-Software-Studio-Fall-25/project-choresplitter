33b3c10da47abfccebfc728775293ce6a7b04b27
Kevin Han <kevinhan@Kevins-MacBook-Air-9.local>
2025-11-20 01:31:23 -0600

    heroku fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
-  allow_unauthenticated_access only: %i[new create]
-  skip_before_action :require_login, :require_authentication, only: [ :new, :create ], raise: false
+  allow_unauthenticated_access only: %i[new create destroy]
+  skip_before_action :require_login, :require_authentication, only: [ :new, :create, :destroy ], raise: false
   # rate_limit to: 10, within: 3.minutes, only: :create,
   #          with: -> { redirect_to new_session_url, alert: "Try again later." }
 
